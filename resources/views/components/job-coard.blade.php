266cb4263d0774f5c308f3a983a5eaae8454e608
John Osmond <john@jtech10.com>
2024-10-16 13:08:26 -0400

    first commit

---
+++ resources/views/components/job-coard.blade.php
@@ -0,0 +1,15 @@
+<div class="p-4 bg-white/5 rounded-lg flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8 font-bold">
+        <h3>Video Producers</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag1</x-tag>
+            <x-tag>Tag2</x-tag>
+            <x-tag>Tag3</x-tag>
+        </div>
+        <img class="rounded-lg" src="https://placehold.it/42/42" alt="">
+    </div>
+</div>
